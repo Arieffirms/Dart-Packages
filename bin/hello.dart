a08381bc8be2476debcff4b9cd69b0991c3911d4
Arieffirms <arieffirmansyah0731@gmail.com>
2024-04-25 14:47:40 +0700

    menambah materi

---
+++ bin/hello.dart
@@ -0,0 +1,10 @@
+import 'package:dart_packages/hello.dart';
+
+void main () {
+  print(sayHello("arief"));
+
+  var customer = Customer();
+  var product = Product();
+  var category = Category();
+
+}
